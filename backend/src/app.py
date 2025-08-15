744769dfacebb826e9cca401ea7d8cc90046dee1
sumity2021 <sumity2021@gmail.com>
2025-08-16 01:48:18 +0530

    cors fixed

--- backend/src/app.py
+++ backend/src/app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Correct CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://tech-quest-tjhf.vercel.app/"],       # You can restrict this to your React frontend URL
+    allow_origins=["*"],       # You can restrict this to your React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
